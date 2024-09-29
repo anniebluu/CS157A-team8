c9f9ca5cb6689885529b15c960aea658bd6b343c
GuadalupeRamirezLara <guadalupe.ramirezlara@sjsu.edu>
2024-09-29 13:46:42 -0700

    Added tables for Users, Pets, PetOrganization, Applications, and Appointments

--- pet_query_dump.sql
+++ pet_query_dump.sql
@@ -1,52 +1,56 @@
 -- MySQL dump 10.13  Distrib 9.0.1, for macos14.4 (arm64)
 --
 -- Host: localhost    Database: Pet Query
+-- Sources: 
+-- https://www.w3schools.com/sql/
+-- https://atlasgo.io/guides/mysql/check-constraint
+-- https://www.geeksforgeeks.org/enumerator-enum-in-mysql/
 -- ------------------------------------------------------
 -- Server version	9.0.1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE Users (
+-- primary key
+Username varchar(50) PRIMARY KEY,
+UsrEmail varchar(50) NOT NULL UNIQUE,
+UsrName varchar(50) NOT NULL,
+UsrPassword varchar(50) NOT NULL,
+AccountType int NOT NULL,
+-- assuming account types are assigned 0 or 1 with each representing either Adopter or Org Admin
+CHECK (AccountType IN (0, 1))
+);
 
---
--- Table structure for table `member`
---
+CREATE TABLE Pets (
+-- primary key
+PetID int PRIMARY KEY,
+PetName varchar(50) NOT NULL,
+Age int NOT NULL,
+Category varchar(50),
+-- uses id of PetOrg as secondary key
+OrgID int,
+FOREIGN KEY (OrgID) REFERENCES PetOrganization(OrgID)
+);
 
-DROP TABLE IF EXISTS `member`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `member` (
-  `uname` varchar(45) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`uname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE PetOrganization (
+-- primary key
+OrgID int PRIMARY KEY,
+Address varchar(50) NOT NULL,
+OrgName varchar(50) NOT NULL,
+OrgEmail varchar(50) NOT NULL
+);
 
---
--- Dumping data for table `member`
---
+CREATE TABLE Applications (
+AppID int PRIMARY KEY,
+Username varchar(50) NOT NULL,
+AppStatus ENUM('pending', 'proccessed') DEFAULT 'pending'
+);
 
-LOCK TABLES `member` WRITE;
-/*!40000 ALTER TABLE `member` DISABLE KEYS */;
-/*!40000 ALTER TABLE `member` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE Appointments (
+Username varchar(50) NOT NULL,
+-- uses username as secondary key
+FOREIGN KEY (Username) REFERENCES Users(Username),
+Date DATE NOT NULL,
+Time TIME NOT NULL,
+-- makes sure no two appointments overlapp for users
+UNIQUE (Date, Time, OrgID, Username)
+);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-09-19 22:15:28
