e37786d5559307b714ac9fe36f1bae84d7ec2bb9
Annie Luu <anniebluu@protonmail.ch>
2024-10-14 21:39:54 -0700

    Update RegisterDao.java

--- src/main/java/edu/sjsu/cs157ateam8/RegisterDao.java
+++ src/main/java/edu/sjsu/cs157ateam8/RegisterDao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class RegisterDao {
 	private String dburl = "jdbc:mysql://localhost:3306/petquery";
 	private String dbuname = "root";
-	private String dbpassword = "Sequelrelationsystem6*";
+	private String dbpassword = "";
     private String dbdriver = "com.mysql.cj.jdbc.Driver";
 
 	public void loadDriver(String dbDriver)
